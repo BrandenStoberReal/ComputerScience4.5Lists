89b342c6989d4e31dadbcb76e62a3652d7377c26
Branden <theeladdergoat@gmail.com>
2022-01-31 14:08:33 -0500

    First 3 objectives

--- ComputerScience4.5Lists/ComputerScience4.5Lists.py
+++ ComputerScience4.5Lists/ComputerScience4.5Lists.py
@@ -6,11 +6,22 @@ Names = ["Peter", "Bruce", "Steve", "Tony", "Natasha", "Clint", "Wanda", "Hope",
 Numbers = [100, 50, 10, 1, 2, 7, 11, 17, 53, -8, -4, -9, -72, -64, -80]
 
 #Objective 1
-Switch = 0
 for i in range(len(Names)):
-    if (Switch == 0):
-        Switch = 1
+    if (i % 2 == 0):
         print(Names[i])
-    else:
-        Switch = 0
 
+print("\n")
+
+#Objective 2
+for i in range(len(Numbers)):
+    if (Numbers[i] > 0):
+        print(Numbers[i])
+
+print("\n")
+#Objective 3
+Sum = 0
+for i in range(len(Numbers)):
+    Sum = Sum + Numbers[i]
+print(Sum)
+
+print("\n")
