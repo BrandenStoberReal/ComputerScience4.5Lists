a9a03fdb576d93c2ba0f9d49eef3b5890feb79b1
Branden <theeladdergoat@gmail.com>
2022-01-31 13:49:02 -0500

    Added env

--- ComputerScience4.5Lists/ComputerScience4.5Lists.py
+++ ComputerScience4.5Lists/ComputerScience4.5Lists.py
@@ -4,4 +4,3 @@ import random
 
 Names = ["Peter", "Bruce", "Steve", "Tony", "Natasha", "Clint", "Wanda", "Hope", "Danny", "Carol"]
 Numbers = [100, 50, 10, 1, 2, 7, 11, 17, 53, -8, -4, -9, -72, -64, -80]
-
