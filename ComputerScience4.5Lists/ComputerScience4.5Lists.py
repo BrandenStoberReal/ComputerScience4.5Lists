83dd2867d28f7897977476cdbaf36094e288300b
Branden <theeladdergoat@gmail.com>
2022-01-31 13:17:00 -0500

    Initial lists from rubric

--- ComputerScience4.5Lists/ComputerScience4.5Lists.py
+++ ComputerScience4.5Lists/ComputerScience4.5Lists.py
@@ -1,1 +1,7 @@
+import os
+import math
+import random
+
+Names = ["Peter", "Bruce", "Steve", "Tony", "Natasha", "Clint", "Wanda", "Hope", "Danny", "Carol"]
+Numbers = [100, 50, 10, 1, 2, 7, 11, 17, 53, -8, -4, -9, -72, -64, -80]
 
